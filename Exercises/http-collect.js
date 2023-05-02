89ceb00ddc7bbeb6d081387c68902f0d902d2bba
Sepy <lacostem@bzz.ch>
2023-05-02 11:49:21 +0000

    Bl installiert leider fehler mit b1 gemacht

--- Exercises/http-collect.js
+++ Exercises/http-collect.js
@@ -1,3 +1,10 @@
 const http = require('http');
 const url = process.argv[2];
 const bl = require('bl');
+http.get(url, function (response) {
+    response.pipe(bl(function (err, data) {
+        if (err) return console.error(err);
+        console.log(data.length);
+        console.log(data.toString());
+    }))
+})
