5133cec8234a0ea4a4a532e99c8a768731ad58af
Sepy <lacostem@bzz.ch>
2023-05-03 07:46:37 +0000

    Request/Response Experiments 1 Beendet

--- express_helloworld/index.js
+++ express_helloworld/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import useragent from 'useragent';
 
 const app = express();
 const port = 3010;
@@ -19,6 +20,35 @@ app.get('/zli', function (req, res) {
 app.get('/name', function (req, res) {
     res.send(names[Math.floor(Math.random() * names.length)]);
 })
+
+app.get('/html', function(req, res) {
+    res.sendFile('/workspaces/M295/express_helloworld/testdatei.html');
+  });
+
+app.get('/image', function(req, res) {
+    res.sendFile('/workspaces/M295/express_helloworld/testbild.jpg');
+});
+
+app.get('/teapot', function(req, res) {
+    res.status(418)
+});
+
+app.get('/user-agent', function(req, res) {
+    var agent = useragent.parse(req.headers['user-agent']);
+    res.send(agent.family);
+});
+
+app.get('/secret', function(req, res) {
+    res.status(403).send('Access denied');
+});
+
+app.get('/xml', function(req, res) {
+    res.sendFile('/workspaces/M295/express_helloworld/test.xml');
+});
+
+app.get('/json', function(req, res) {
+    res.sendFile('/workspaces/M295/express_helloworld/test.json');
+});
 
 
 app.listen(port, function () {
