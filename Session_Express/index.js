8f62bfb023c56266cf21629623b6d41cdf7ac3f5
Sepy <lacostem@bzz.ch>
2023-05-04 14:56:24 +0000

    Added Login

--- Session_Express/index.js
+++ Session_Express/index.js
@@ -1,8 +1,34 @@
 import express from "express";
+import session from "express-session";
 
 const app = express();
-const session = require('express-session');
-const port = 3000;
+const port = 2000;
+
+app.use(express.json());
+app.use(session({
+    secret: 'secret',
+    resave: false, 
+    saveUninitialized: false,
+    cookies: {},
+}));
+
+const Admincredentials = {    
+    email: 'antip', 
+    password: '1234' 
+};
+
+app.post('/login',(req, res) => {
+    const email = req.query.email;
+    const password = req.query.password;
+    
+    if (email == Admincredentials.email && password == Admincredentials.password) {
+        return res.status(200).send('Login successful');
+    } else {
+    return res.status(401).send('Login failed!');
+    }
+});
+
+
 
 
 
